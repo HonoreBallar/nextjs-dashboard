d8f3bd1250b609637240de00aee4b7a5178284b8
Amounan Honoré <amounahonore@gmail.com>
2025-07-28 12:46:46 +0000

    update file again

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import postgres from 'postgres';
-import { invoices, customers, revenue, users } from '../lib/placeholder-data__old.ts';
+import { invoices, customers, revenue, users } from '../lib/placeholder-data.ts';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
